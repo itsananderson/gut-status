99d27b1f01695bf6e4b3fee4243756e60ab918d7
Will Anderson <will@itsananderson.com>
2014-07-03 21:14:04 +0000

    Spawn 'git status' with provided args

--- bin/gut-cli.js
+++ bin/gut-cli.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 var gut = require('../');
+var spawn = require('child_process').spawn;
 
 if (process.argv.length > 2 && process.argv[2] === 'status') {
     console.log("\n\tGut status: %s", gut.getStatus());
+    var child = spawn('git', Array.prototype.slice.call(process.argv, 2));
+    child.stdout.pipe(process.stdout);
+} else {
+    console.log('\nI think you meant "git"');
 }
-
-console.log('\nI think you meant "git"');
