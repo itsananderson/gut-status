179c488c0cc7f0883cd4ced27f1bee60ec2c9dba
Will Anderson <will@itsananderson.com>
2015-05-19 22:43:26 -0700

    Color gut status green

--- bin/gut-cli.js
+++ bin/gut-cli.js
@@ -2,9 +2,10 @@
 
 var gut = require('../');
 var spawn = require('child_process').spawn;
+var colors = require('colors/safe');
 
 if (process.argv.length > 2 && process.argv[2] === 'status') {
-    console.log("\n\tGut status: %s", gut.getStatus());
+    console.log("\n\tGut status: %s\n", colors.green(gut.getStatus()));
     var child = spawn('git', Array.prototype.slice.call(process.argv, 2));
     child.stdout.pipe(process.stdout);
 } else {
